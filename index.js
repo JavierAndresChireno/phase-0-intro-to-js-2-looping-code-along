16dd61d5d8bf1c0de613d7cb175b3736120e9c14
Javier Chireno <javierchireno04@gmail.com>
2021-07-09 15:59:26 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+
+const names = ['Lisa', 'Kaitlin','Jan']
+const party = ['Surprise']
+
+function writeCards(names, event){
+    let newArr = [];
+    for (let i = 0; i < names.length; i++){
+        newArr.push (`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+    return newArr;
+}
+writeCards(['Lisa','Kaitlin','jan'],'surprise');
+
+function countDown(i){
+    while (i >=0){
+        console.log(i);
+        i--;
+;    }
+
+}
